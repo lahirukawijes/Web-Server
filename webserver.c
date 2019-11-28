1f2dc2e98feb436ef1b72ab5f87fcb76d2e557d3
lahirukawijes <lahiruka.wijesinghe@gmail.com>
2019-11-28 07:59:58 +0530

    initial commit for server

---
+++ webserver.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+
+int main(argc, char *argv[]){
+
+
+    return 0;
+}
